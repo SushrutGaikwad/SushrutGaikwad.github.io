bd958630e2bd5ee770121beb5aa8231c39b61acf
SushrutGaikwad <88122573+SushrutGaikwad@users.noreply.github.com>
2025-06-14 04:59:54 +0000

    Deploying to gh-pages from @ SushrutGaikwad/SushrutGaikwad.github.io@13b99ed7416dc57daa43a7b08c03e441cc19957f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-mcculloch-pitts-neuron",
+        
+          title: "McCulloch-Pitts Neuron",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/mcculloch-pitts-neuron/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
